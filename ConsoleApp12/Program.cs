144895a7a97c26d6104a3331942c586f7e9aad62
admin <admin@DESKTOP-NH7NBB7>
2022-10-14 23:40:59 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,11 @@
+﻿while (true)
+{
+    int[] octave1 = new int[] { 650, 690, 730, 770, 820, 870, 920, 980 };
+    int[] octave2 = new int[] { 1300, 1380, 1460, 1550, 1640, 1740, 1850, 1960 };
+    ConsoleKeyInfo key = Console.ReadKey();
+
+
+
+
+}
+
